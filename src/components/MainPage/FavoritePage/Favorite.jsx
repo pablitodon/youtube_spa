7948ae707c2d6bc85b382773f5e37a9d623101e2
pablitodon <pavelkazakevich040196@gmail.com>
2024-10-03 21:12:00 +0300

    fix apibug

--- src/components/MainPage/FavoritePage/Favorite.jsx
+++ src/components/MainPage/FavoritePage/Favorite.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { setResultText } from "../../redux/slices/resultTextSlice";
 import { toggleModal } from "../../redux/slices/visibleModalSlice";
 import ModalComponent from "../Modal/Modal";
-import  { editDataFunc } from "../../redux/slices/editData";
+import { editDataFunc } from "../../redux/slices/editData";
 import './favorite.css'
 import { useEffect } from "react";
 import { allStartResponse } from "../../redux/slices/saveRequestSlice";
@@ -16,56 +16,59 @@ const Favorite = () => {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
-    const  saveRequests  = useSelector((state) => state.saveRequests);
+    const saveRequests = useSelector((state) => state.saveRequests);
     const user = localStorage.getItem('user')
+    console.log(saveRequests);
 
 
     useEffect(() => {
-        dispatch(allStartResponse(JSON.parse(localStorage.getItem(`${user}`))))
-    },[])
+        const storedRequests = JSON.parse(localStorage.getItem(`${user}`));
+        if (storedRequests) {
+            dispatch(allStartResponse(storedRequests));
+        }
+    }, []);
 
 
-    
-    const handleRequestExecute =  (query, maxResults,order) => {
+    const handleRequestExecute = (query, maxResults, order) => {
         try {
             dispatch(setResultText(query))
-           dispatch(fetchGetSearch({ query: query, maxResults: maxResults, order: order}));
+            dispatch(fetchGetSearch({ query: query, maxResults: maxResults, order: order }));
             navigate('/main/search');
         } catch (error) {
             console.error("Error fetching search results:", error);
         }
     }
-    
 
 
-    
-    const handleOpenModalRedactor =  (id) => {
+
+
+    const handleOpenModalRedactor = (id) => {
         const selectedItem = saveRequests.find(video => video.id === id);
-    
+
         if (selectedItem) {
             dispatch(editDataFunc(selectedItem));
             dispatch(toggleModal());
         }
     };
- 
 
-    return (    
-            <div className="favorite-container">
-                <h2>Избранное:</h2>
+
+    return (
+        <div className="favorite-container">
+            <h2>Избранное:</h2>
             <div className="favorite-title">
                 <div>
                     {
                         saveRequests.map((video) => (
                             <div key={video.id} className="video-item-favorite">
-                                <p style={{padding:'10px'}}>{video.type}</p>
-                                <div style={{padding:'10px'}}>
-                                    <Button onClick={() => handleRequestExecute(video.query,video.maxResults,video.sortBy)}>
+                                <p style={{ padding: '10px' }}>{video.type}</p>
+                                <div style={{ padding: '10px' }}>
+                                    <Button onClick={() => handleRequestExecute(video.query, video.maxResults, video.sortBy)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" style={{ width: '20px' }} stroke="#000">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.91 11.672a.375.375 0 0 1 0 .656l-5.603 3.113a.375.375 0 0 1-.557-.328V8.887c0-.286.307-.466.557-.327l5.603 3.112Z" />
                                         </svg>
                                     </Button>
-                                    <Button   onClick={() => handleOpenModalRedactor(video.id)}>
+                                    <Button onClick={() => handleOpenModalRedactor(video.id)}>
                                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" style={{ width: '20px' }} stroke="#000">
                                             <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
                                         </svg>
@@ -77,7 +80,7 @@ const Favorite = () => {
                 </div>
             </div>
             <div>
-                <ModalComponent/>
+                <ModalComponent />
             </div>
         </div>
 
