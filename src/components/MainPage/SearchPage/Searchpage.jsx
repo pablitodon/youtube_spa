bc1f5558ec45de202cac6b94939a2c0db761b6d1
pablitodon <pavelkazakevich040196@gmail.com>
2024-10-03 20:24:57 +0300

    fix app

--- src/components/MainPage/SearchPage/Searchpage.jsx
+++ src/components/MainPage/SearchPage/Searchpage.jsx
@@ -17,7 +17,6 @@ const SearchPage = () => {
     const { items, status } = useSelector(state => state.youtubeSearch);
 
 
-
     const handleChange = (e) => {
         setSearchTextInput(e.target.value)
     }
